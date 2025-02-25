6ec4866859e97d0d92f5b464203959506daa8267
Collum <collum.freedman@gmail.com>
2025-02-25 12:49:00 -0500

    Implement basic class methods for router trie structure with static and dynamic routes added and searched for within the existing route records

--- src/index.js
+++ src/index.js
@@ -86,6 +86,175 @@ class Router {
   }
 }
 
+class TrieNode {
+  constructor() {
+    this.children = new Map();
+    this.isWordEnd = false;
+  }
+
+  add(letter, _isLastLetter) {
+    let newNode = new TrieNode();
+    this.children.set(letter, newNode);
+
+    newNode.isWordEnd = _isLastLetter;
+    return newNode;
+  }
+}
+
+class Trie {
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  insert(word, node=this.root) {
+    const wordLength = word.length;
+    if (wordLength == 0) {
+      return
+    }
+
+    let firstNode = node.children.get(word[0]);
+
+    if (firstNode) {
+      this.insert(word.slice(1), firstNode)
+    } else {
+      let newNode = node.add(word[0], length == 1);
+      this.insert(word.slice(1), newNode);
+    }
+  }
+
+  search(word, node=this.root) {
+    if (word.length == 0) {
+      return node.isWordEnd
+    }
+
+    if (!node.children.has(word[0])) {
+      return false
+    }
+
+    return this.insert(word.slice(1), node.children.get(word[0]))
+  }
+}
+
+class RouteNode {
+  constructor() {
+    this.children = new Map();
+    this.handler = new Map();
+    this.params = [];
+  }
+}
+
+class TrieRouter {
+  constructor() {
+    this.root = new RouteNode();
+  }
+
+  #verifyParams(path, method, handler) {
+    if (!path || typeof path !== "string" || path[0] !== "/") {
+      throw new Error(`Incorrectly specified path ${path} - path should be string beginning with a slash mark`);
+    }
+    if (typeof handler !== "function") {
+      throw new Error("Incorrectly specified handler function - it should be of function type")
+    }
+    if (!HTTP_METHODS[method]) {
+      throw new Error("Invalid HTTP method supplied")
+    }
+  }
+
+  #addRoute(path, method, handler) {
+    this.#verifyParams(path, method, handler);
+
+    let path = path.strip("/");
+    const segments = path.split("/");
+    let dynamicParams = [];
+
+    if (segments.some((segment) => segment.includes(" "))) {
+      throw new Error(`Incorrectly specified path ${path} - path should contain no whitespace`)
+    }
+
+    let currentNode = this.root;
+    for (let segment of segments) {
+      const isDynamic = segment[0] === ":";
+      const key = isDynamic ? ":": segment;
+
+      if (isDynamic) {
+        dynamicParams.push(segment.slice(1));
+      }
+
+      if (!currentNode.children.has(key)) {
+        let newNode = new RouteNode();
+        currentNode.children.set(key, newNode);
+      }
+
+      currentNode = currentNode.children.get(segment);
+    }
+
+    currentNode.handler.set(method, handler);
+    currentNode.params = dynamicParams;
+  }
+
+  findRoute(path, method) {
+    const segments = path.strip("/").split("/");
+
+    let currentNode = this.root;
+    let extractedParams = [];
+
+    for (let segment of segments) {
+      if (!currentNode.children.has(segment)) {
+        return null
+      }
+
+      currentNode = currentNode.children.get(segment)
+    }
+
+    return currentNode.handler.get(method);
+  }
+
+  printTree(node = this.root, indentation = 0) {
+    const indent = "-".repeat(indentation);
+
+    node.children.forEach((childNode, segment) => {
+      console.log(`${indent}${segment}`);
+      this.printTree(childNode, indentation + 1);
+    });
+  }
+
+  get(path, handler) {
+    this.#addRoute(path, HTTP_METHODS.GET, handler);
+  }
+
+  post(path, handler) {
+    this.#addRoute(path, HTTP_METHODS.POST, handler);
+  }
+
+  put(path, handler) {
+    this.#addRoute(path, HTTP_METHODS.PUT, handler);
+  }
+
+  patch(path, handler) {
+    this.#addRoute(path, HTTP_METHODS.PATCH, handler);
+  }
+
+  head(path, handler) {
+    this.#addRoute(path, HTTP_METHODS.HEAD, handler);
+  }
+
+  delete(path, handler) {
+    this.#addRoute(path, HTTP_METHODS.DELETE, handler);
+  }
+
+  options(path, handler) {
+    this.#addRoute(path, HTTP_METHODS.OPTIONS, handler);
+  }
+
+  trace(path, handler) {
+    this.#addRoute(path, HTTP_METHODS.TRACE, handler);
+  }
+
+  connect(path, handler) {
+    this.#addRoute(path, HTTP_METHODS.CONNECT, handle)
+  }
+}
+
 const router = new Router();
 router.get("/", function handleGetBasePath(req, res) { 
   console.log("Hello from GET /"); 
